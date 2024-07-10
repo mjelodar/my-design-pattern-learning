c2330ce264c86defa0fdf980ce79acaa7e7477eb
mjelodar <jelodarian.92@gmail.com>
2024-07-10 13:04:45 +0330

    this is an example from https://refactoring.guru/design-patterns/state/java/example

---
+++ state/src/StateDemo.java
@@ -0,0 +1,10 @@
+import ui.Player;
+import ui.UI;
+
+class StateDemo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
